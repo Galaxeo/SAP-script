df818af9fbdbea867ff63efc9470670c0c13c4a4
Galaxeo <hwjustincheok@gmail.com>
2021-07-06 01:40:07 -0700

    Update spreadsheet test.py

--- spreadsheet test.py
+++ spreadsheet test.py
@@ -10,10 +10,9 @@ scope = [
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
+# Open spreadsheet by using the key in the URL
 sheet = client.open_by_key("1fy6qiIv6CoGAG4C7EOaUrRgKjJBttqCsQoQhwZzgXU8").sheet1
-# Extract and print all of the values
+# Extract values into a dictionary
 list_of_hashes = sheet.get_all_records()
 for item in list_of_hashes:
     print(item["Phone IP"], "Section", item["Section"], "Row", item["Row"])
