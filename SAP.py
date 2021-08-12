57e33bd54d1ea5f0e698dd8970dfe4556b68c085
Justin Cheok <hwjustincheok@gmail.com>
2021-08-12 14:11:07 -0400

    Fixed file open, updated CSV to July 20th/Aug 9th 2021 csv. Please keep in mind that acceptable forms are as such: 1023, 213, 214, ...
    or
    
    172.16.0.251
    172.16.1.20
    172.16.0.235
    172.16.2.34
    ...

--- SAP.py
+++ SAP.py
@@ -1,15 +1,19 @@
 from operator import itemgetter
 import csv
-f = open(r"C:\Users\Justin\Downloads\UE Locations for AT&T and pCell Testing at SAP  - June 29, 2021.csv", "r")
+f = open(r"C:\Users\Justin Cheok\Downloads\UE Locations for AT&T and pCell Testing at SAP  - July 20 - Aug 9, 2021.csv", "r")
 csv_f = csv.reader(f)
 datalist = list(csv_f)
 lst = []
 returnlst = []
+leftatlab = []
+brokenphones = []
 print("Enter the # in a comma seperated list or the IPs, then enter 'done' when done: ")
 while True:
     inp = input()
     if inp.lower() == "done":
         break
+    elif inp == "":
+        continue
     elif "," in inp:
         lst = inp.split(", ")
         lst = sorted(lst)
@@ -33,36 +37,42 @@ for row in datalist:
         continue
     for num in lst:
         if row[3] == num:
-            returnlst.append([num, "Sect. ", row[5], "Row ", row[7], "Pos. ", row[8]])
-            lst.remove(num)
+            if int(num) in brokenphones:
+                lst.remove(num)
+                continue
+            elif int(num) in leftatlab:
+                returnlst.append([num, "Left at Lab"])
+            else:
+                returnlst.append([num, "Sect. ", row[5], "Row ", row[6], "Pos. ", row[7]])
+                lst.remove(num)
         else:
             continue
 for item in lst:
     returnlst.append([item, "Sect. ", "ERROR ", "Row ", "ERROR ","Pos. ","ERROR "])
 number = 0
 #I use itemgetter here to sort by the section of the stadium by default
-returnlst = sorted(returnlst, key=itemgetter(2))
-for item in returnlst:
-    print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
-    number+=1
-if counter == number:
-    print("Good to go")
-else:
-    print("error somewhere")
+# returnlst = sorted(returnlst, key=itemgetter(2))
+# for item in returnlst:
+#     print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
+#     number+=1
+# if counter == number:
+#     print("Good to go")
+# else:
+#     print("error somewhere")
 #Additional code if people want to see phones by row, section, or sort the list by number
-# sortask = input("row, section, or #(number) ")
-# if sortask.lower() == "section":
-#     #Below is categorized by section
-#     returnlst = sorted(returnlst, key=itemgetter(2))
-#     for item in returnlst:
-#         print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
-#         number+=1
-# elif sortask.lower() == "row":
-#     #Below is categorized by row
-#     returnlst = sorted(returnlst, key=itemgetter(4))
-#     for item in returnlst:
-#         print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
-# elif sortask.lower() == "#" or sortask.lower() == "number":
-#     returnlst = sorted(returnlst, key=itemgetter(0))
-#     for item in returnlst:
-#         print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
+sortask = input("row, section, or #(number) ")
+if sortask.lower() == "section":
+    #Below is categorized by section
+    returnlst = sorted(returnlst, key=itemgetter(2))
+    for item in returnlst:
+        print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
+        number+=1
+elif sortask.lower() == "row":
+    #Below is categorized by row
+    returnlst = sorted(returnlst, key=itemgetter(4))
+    for item in returnlst:
+        print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
+elif sortask.lower() == "#" or sortask.lower() == "number":
+    returnlst = sorted(returnlst, key=itemgetter(0))
+    for item in returnlst:
+        print(item[0], item[1] + item[2], item[3] + item[4], item[5] + item[6])
